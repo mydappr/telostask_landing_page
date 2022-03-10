b7657537c39afdfb21c9a8a0418e7c652fd01f1b
solomon stephen <33923021+realrufans@users.noreply.github.com>
2022-03-10 23:39:12 +0100

    changed Featured articles

--- src/containers/Featured.js
+++ src/containers/Featured.js
@@ -1,11 +1,11 @@
 export const Featured = [
   {
-    Author: "@leviet7490",
+    Author: "Eosgo.io",
     thumbnail:
-      "https://images.hive.blog/DQmUrxd3knH7oqsh1dyaHuXpLcMT12SoPkiriv2cFoK9Bzd/image.png",
-    Date: "December 12, 2020",
-    Title: "All You Need To Know About The TelosTask Project",
-    link: "https://leofinance.io/@leviet7490/all-you-need-to-know-about-the-telostask-project",
+      "https://images.hive.blog/DQmcMtiEaGzgZKx8WJCvdPVeN6ywPGsV64aD9VJocAjm2oX/image.png",
+    Date: "2020-10-30",
+    Title: "New Gig Economy Platform Launched on Telos: TelosTask",
+    link: "https://www.eosgo.io/news/new-gig-economy-launched-on-telos-telostask",
   },
   {
     Author: "MARIE HUILLET",
@@ -16,10 +16,10 @@ export const Featured = [
     link: "https://cointelegraph.com/news/telos-launches-new-gig-economy-platform-on-the-blockchain",
   },
   {
-    Author: "Staff Writer",
-    thumbnail: "https://images.hive.blog/DQmWA3StFQk2Ct6K43qoScQg3mVEZzHeW49khcMRkurF7CL/image.png",
-    Date: "7th Feb 2022",
-    Title: "Blockchain platform Telos launches gig economy tool",
-    link: "https://www.banklesstimes.com/news/2020/10/28/blockchain-platform-telos-launches-gig-economy-tool/",
+    Author: "Dappradar.com",
+    thumbnail: "https://images.hive.blog/DQmbqPXh4K9tefmLmWETyyH9gxov8qHJaiPU3eeUVNZfBFW/image.png",
+    Date: " ",
+    Title: "What is TelosTask?",
+    link: "https://dappradar.com/telos/marketplaces/telostask",
   },
 ];
